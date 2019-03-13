e56c05086da6b903dff1069c59ebe6964e58bd0a
Syzygianinfern0 <spsharan2000@gmail.com>
2019-03-13 19:12:35 +0530

    Brackets are irratating

--- wikitest.py
+++ wikitest.py
@@ -1,39 +1,36 @@
 import wikipedia
-x = "Modi"
-x = wikipedia.summary(x)
 
-x = '.'.join(x.split('.')[:3]) + '.'
-x = 'hey (this is (sharan and his (useless) friend) asus tuf).'
+query = "Austria"
+query_bak = query
+query = wikipedia.summary(query)
+query = '.'.join(query.split('.')[:3]) + '.'
 
-"""for line in x.split('.'):
-    c = 0
-    while True:
-        s = line.find('(')
-        if s is -1: break
-        c = 1
-        i = 0
-        for ch in line:
-            i += 1
-            if c is 0:
-                break
-            if ch is '(':
-                c += 1
-            elif ch is ')':
-                c -= 1
-        line = line[:s-1] + line[i+1:]
-    print(line)"""
-
-
-ret = ''
+"""ret = ''
 skip1c = 0
 skip2c = 0
-for i in x:
+for i in query:
     if i == '(':
         skip1c += 1
     elif i == ')'and skip2c > 0:
         skip2c -= 1
     elif skip1c == 0 and skip2c == 0:
-        ret += i
+        ret += i"""
 
+ret = ''
+skip1c = 0
+skip2c = 0
+for i in query:
+    if i == '[':
+        skip1c += 1
+    elif i == '(':
+        skip2c += 1
+    elif i == ']' and skip1c > 0:
+        skip1c -= 1
+    elif i == ')'and skip2c > 0:
+        skip2c -= 1
+    elif skip1c == 0 and skip2c == 0:
+        ret += i
 print(ret)
+
+
 
